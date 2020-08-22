2128412033af234ddec0473a7dc18b8e6286b143
n-takao <ren615myu072@gmail.com>
2020-08-22 07:52:51 +0000

    PHP/Laravel 19 課題

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment('production')) {
-        \URL::forceScheme('https');
-    }
+         if (\App::environment('production')) {
+         \URL::forceScheme('https');
+     }
     
     }
 }
